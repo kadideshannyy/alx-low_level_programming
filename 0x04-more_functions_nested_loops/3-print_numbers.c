09cf9bfc9a9288c4a6b8da7edd171623dcd01876
kadideshannyy <kadideshannyy@gmail.com>
2023-02-22 22:26:14 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * pritn_numbers - prints numbers
+ * @void: Return nothing
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+int i:
+
+for (i =48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
